4559f084d66be80e0d4779096290b840c21d0685
Davila-Israel96 <israeldavila96@gmail.com>
2023-03-02 21:15:16 -0600

    server is working, and React app can fetch from it

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const openAiRouter = require('./routes/openAi');
+app.use('/gpt', openAiRouter);
+
 app.listen(PORT, () => {
 	console.log(`server listening on ${PORT}`);
 });
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
+
+app.get('/text', (req, res) => {
+	res.json({ data: 'this is data' });
 });
